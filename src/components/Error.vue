ea25cfe7da6317aa94401fd5bbcfde8cf8148e5c
Tiam <3036293856@qq.com>
2023-01-16 16:52:16 +0800

    优化了一点错误界面

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="error">
-        <span>请求出错啦, 请检查网络</span>
-        <img src="../assets/MEnk3.gif" alt="" />
+        <center>
+            <span>好像出错了呢</span>
+        </center>
+        <center>
+            <img src="../assets/MEnk3.gif" alt="" />
+        </center>
     </div>
 </template>
 
@@ -15,5 +19,10 @@ export default {
 .error img {
     width: 500px;
     height: 500px;
+}
+.error span {
+    font-size: 30px;
+    font-weight: bold;
+    color: #f5587b;
 }
 </style>
